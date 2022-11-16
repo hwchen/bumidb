3192a0f771c756388254de1d4f5bd0ee4a61ada7
Walther Chen <walther.chen@gmail.com>
2022-11-15 21:56:27 -0500

    build w/ rocksdb

--- build.zig
+++ build.zig
@@ -5,13 +5,21 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("bumidb", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
-
     const main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+    // kv executable
+    const kv_exe = b.addExecutable("kv", "./scratch/kv.zig");
+    kv_exe.addPackagePath("bumidb", "./src/main.zig");
+    kv_exe.linkLibC();
+    kv_exe.linkSystemLibraryName("rocksdb");
+    kv_exe.addLibraryPath("./rocksdb");
+    kv_exe.addIncludePath("./rocksdb/include");
+    kv_exe.setBuildMode(mode);
+    kv_exe.install();
+    const kv_step = b.step("kv", "Build kv cli");
+    kv_step.dependOn(&kv_exe.step);
 }
---
+++ scratch/kv.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+const rdb = @cImport(@cInclude("rocksdb/c.h"));
+
+pub fn main() anyerror!void {
+    var db = try RocksDb.open("data");
+    defer db.close();
+    std.debug.print("Opened db?", .{});
+}
+
+pub const RocksDb = struct {
+    db: *rdb.rocksdb_t,
+
+    pub fn open(dir: []const u8) !RocksDb {
+        var options = rdb.rocksdb_options_create();
+        rdb.rocksdb_options_set_create_if_missing(options, 1);
+
+        var err: ?[*:0]u8 = null;
+        var db = rdb.rocksdb_open(options, dir.ptr, &err);
+        if (err) |e| {
+            std.debug.print("Error: {s}", .{e});
+            return error.RocksDbOpen;
+        }
+
+        return .{ .db = db orelse return error.RocksDbFail };
+    }
+
+    pub fn close(self: RocksDb) void {
+        rdb.rocksdb_close(self.db);
+    }
+};
